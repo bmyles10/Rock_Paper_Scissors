ccb24e95eb40d43124af509718475b18e855a5b0
Brandon Myles <bmyles1006@gmail.com>
2019-02-14 15:59:09 -0600

    Finished Product

--- RPS.js
+++ RPS.js
@@ -2,7 +2,7 @@ let cpuScore = 0;
 let playScore = 0;
 
 let welcomeMessage = () => {
-    alert `Welcome to my game of Rock Paper Scissors!, Please click ok to begain and make your choice!`
+    alert `Welcome to my game of Rock Paper Scissors! Please click ok to begain and make your choice!`
 }
 
 let computerPlay = () => {
@@ -24,29 +24,28 @@ let playerSelection = () => {
 
 let playRound = (computerSelection, selection) => {
     if (computerSelection === 'paper' && selection === 'rock') {
-        cpuScore++
-        alert `You choose rock, the CPU choose paper, you LOSE!`;
+        alert ("You choose rock, the CPU choose paper, you LOSE! Score is: You " + playScore + " CPU " + cpuScore);
         return 2;
     } else if (computerSelection === 'rock' && selection === 'scissors') {
-        alert `You choose scissors, the CPU choose rock, you LOSE!`;
+        alert ("You choose scissors, the CPU choose rock, you LOSE! Score is: You " + playScore + " CPU " + cpuScore);
         return 2;
     } else if (computerSelection === 'scissors' && selection === 'paper') {
-        alert `You choose paper, the CPU choose scissors, you LOSE!`;
+        alert("You choose paper, the CPU choose scissors, you LOSE! Score is: You " + playScore + " CPU " + cpuScore);
         return 2;
     } else if (selection === 'paper' && computerSelection === 'rock') {
-        alert `You choose rock, the CPU choose paper, you WIN!`;
+        alert ("You choose rock, the CPU choose paper, you WIN! Score is: You " + playScore + " CPU " + cpuScore);
         return 1;
     } else if (selection === 'rock' && computerSelection === 'scissors') {
-        alert `You choose rock, the CPU choose scissors, you WIN!`;
+        alert ("You choose rock, the CPU choose scissors, you WIN! Score is: You " + playScore + " CPU " + cpuScore);
         return 1;
     } else if (selection === 'scissors' && computerSelection === 'paper') {
-        alert `You choose scissors, the CPU choose paper, you WIN!`;
+        alert ("You choose scissors, the CPU choose paper, you WIN! Score is: You " + playScore + " CPU " + cpuScore);
         return 1;
     } else if (selection === computerSelection) {
-        alert `You both chose the same thing, TIE!`;
+        alert("TIE! Score is: You " + playScore + " CPU " + cpuScore);
         return 0;
     } else {
-        alert ("Invalid response, please enter again.");
+        alert("Invalid response, please enter again.");
         return 0;
 
     }
@@ -65,6 +64,11 @@ let updateScores = (result, points) => {
     }
 }
 
+let clearGame = () => {
+    let cpuScore = 0;
+    let playScore = 0;
+}
+
 
 
 let game = (numofRounds) => {
@@ -74,16 +78,18 @@ let game = (numofRounds) => {
         let puter = computerPlay()
         let result = playRound(puter, playa);
         updateScores(result, 5);
-        if (result != 0){
-            numofRounds--;  
+        if (result != 0) {
+            numofRounds--;
         }
-    } while(numofRounds > 0);
+    } while (numofRounds > 0);
 
-    if (playScore > cpuScore){
-        alert `You scored ${playScore} and the CPU scored ${cpuScore}, you win boiiii!!!!`
+    if (playScore > cpuScore) {
+        alert("You scored " + playScore + " and the CPU scored " + cpuScore + ", you win boiiii!!!!")
     } else {
-        alert `The CPU scored ${cpuScore} and you scored ${playScore}, you lose MF.`
-    }
+        alert("The CPU scored " + cpuScore + " and you scored " + playScore + ", you lose MF.")
+    };
+
+    clearGame();
 }
 
 game(5);
